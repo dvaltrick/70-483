58722e2182cc6e7de60037d659c91f9bd24371c4
dvaltrick <dvaltrick@gmail.com>
2016-09-25 11:18:52 -0300

    1.21

---
+++ Capitulo1/Exemplo 1.19/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//Escalabilidade VS Responsividade
+namespace Exemplo_1._19
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Executa();
+
+            Console.ReadKey();
+        }
+
+        public static async void Executa()
+        {
+            await SleepAsyncA(5000);
+            await SleepAsyncB(5000);
+        }
+
+        public static Task SleepAsyncA(int millisecondsTimeout) {
+            //Executa um Sleep parando a execução da Thread
+            return Task.Run(() => {
+                Thread.Sleep(millisecondsTimeout);
+                Console.WriteLine("Task 1");
+            });
+        }
+
+        public static Task SleepAsyncB(int millisecondsTimeout) {
+            //Nesse método por sua vez, a Thread não é ocupada enquanto o timer aguarda o tempo passado
+            //O que gera um ganho de escalabilidade
+
+            //Cria controle da Task
+            TaskCompletionSource<bool> tcs = null;
+
+            //Define Timer e delega função para ser executada por ele quando atingir o tempo
+            var t = new Timer(delegate { tcs.TrySetResult(true); }, null, -1, -1);
+
+            //Atribui ao controle o resultado do Timer
+            tcs = new TaskCompletionSource<bool>(t);
+            t.Change(millisecondsTimeout, -1);
+
+            Console.WriteLine("Task 2");
+
+            return tcs.Task; 
+        }
+    }
+}
---
+++ Capitulo1/Exemplo 1.20/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Usando ConfigureAwait para tratar erros 
+namespace Exemplo_1._20 //Exemplo 1.20 e 1.21
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Evento de clique do botão
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            //Define um objeto HttpClient
+            HttpClient httpClient = new HttpClient();
+
+            //Executa GetStringAsync para buscar HTML da página
+            string content = await httpClient.
+                GetStringAsync("http://microsoft.com").
+                ConfigureAwait(false);
+
+            //--------Trecho referente ao Exemplo 1.21----------------------
+            //Define bloco using para escrever o arquivo temporário
+            using (FileStream sourceStream = new FileStream("temp.html",
+                FileMode.Create, FileAccess.Write, FileShare.None,
+                4096, useAsync: true))
+            {
+                //Codifica no formato de bytes o resultado da variável content
+                byte[] encodedText = Encoding.Unicode.GetBytes(content);
+
+                //Escreve no disco, no arquivo criado e referenciado no sourceStream
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length).
+                    ConfigureAwait(false);
+            }
+            //--------------------------------------------------------------
+
+            //Força erro setando o conteúdo buscado nessa Thread a Label que está
+            //definida em outra Thread
+            label.Content = content;
+        }
+    }
+}
