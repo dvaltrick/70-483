374ce55033a27d213ac71eecf51e8ec371851cc5
dvaltrick <dvaltrick@gmail.com>
2016-09-26 20:48:28 -0300

    1.27

---
+++ Capitulo1/Exemplo 1.22/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Trabalhando com PLINQ
+//Parallel Language Integrated Query 
+namespace Exemplo_1._22 //Exemplos 1.22 e 1.23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define um array de números
+            var numbers = Enumerable.Range(0, 100000);
+
+            //Executa uma clausula WHERE para apenas apresentar números pares
+            var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0).ToArray();
+
+            foreach (int i in parallelResult) {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Capitulo1/Exemplo 1.26/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Usando ForAll
+namespace Exemplo_1._26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 50);
+            var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0);
+
+            //Utiliza for all para executar uma ação para cada elemento contido na Query
+            parallelResult.ForAll(e => Console.WriteLine(e));
+
+            Console.ReadKey();
+        }
+    }
+}
