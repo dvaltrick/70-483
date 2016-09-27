c1686702db298c5d99bf0c4aac2b7f3c968654f2
dvaltrick <dvaltrick@gmail.com>
2016-09-27 20:52:47 -0300

    1.31

---
+++ Capitulo1/Exemplo 1.29/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Usando GetConsumingEnumerable em uma BlockingCollection
+namespace Exemplo_1._29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define uma BlockingCollection para que possa ser utilizada por duas Tasks diferentes, ao mesmo tempo
+            //realizando tratamento de concorrência
+            BlockingCollection<string> col = new BlockingCollection<string>();
+
+            Task read = Task.Run(() =>
+            {
+                //Utiliza método GetConsumingEnumerable para consumir o item da coleção
+                foreach (string v in col.GetConsumingEnumerable())
+                    Console.WriteLine(v); 
+            });
+
+            Task write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s)) break;
+                    col.Add(s);//Adiciona elemento a coleção
+                }
+            });
+
+            write.Wait();
+        }
+    }
+}
